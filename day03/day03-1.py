d532e35eda06b5cb2a56721970afcd6eaa3a9957
rjuchnicki <robert.juchnicki@gmail.com>
2017-11-23 15:16:25 -0800

    Day 3 solutions

---
+++ day03/day03-1.py
@@ -0,0 +1,15 @@
+f = open('day03.txt', 'r')
+s = f.read()
+f.close()
+
+dirs = {'^': (0,1), 'v': (0,-1), '<': (-1,0), '>': (1,0)}
+
+pos = (0,0)
+visited = set()
+visited.add((0,0))
+for c in s:
+	dx,dy = dirs[c]
+	pos = (pos[0] + dx, pos[1] + dy)
+	visited.add(pos)
+
+print(len(visited))
---
+++ day03/day03-2.py
@@ -0,0 +1,15 @@
+f = open('day03.txt', 'r')
+s = f.read()
+f.close()
+
+dirs = {'^': (0,1), 'v': (0,-1), '<': (-1,0), '>': (1,0)}
+
+pos = [(0,0), (0,0)]
+visited = set()
+visited.add((0,0))
+for i in range(len(s)):
+	dx,dy = dirs[s[i]]
+	pos[i%2] = (pos[i%2][0] + dx, pos[i%2][1] + dy)
+	visited.add(pos[i%2])
+
+print(len(visited))
